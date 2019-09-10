bbfce1b775993906c8557857da911915b22787dc
nk00374 <nk00374@surrey.ac.uk>
2019-09-10 18:24:12 +0100

    Product model template, changed variables to string

---
+++ app/src/main/java/com/example/epos/Models/Product.java
@@ -0,0 +1,49 @@
+package com.example.epos.Models;
+
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    private String barcode;
+    private String name;
+    private String category;
+    private String price;
+
+    public Product(String barcode, String name, String category, String price) {
+        this.barcode = barcode;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
